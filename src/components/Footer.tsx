c4be8e02cd8c95611b6761fe2996c93340ce4bc7
naren4488 <narendrakajla77@gmail.com>
2024-05-04 23:51:00 +0530

    Footer implemented

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,14 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className=" flex justify-between text-sm text-gray-500">
+      <p>@ 2024, ProfileScreener.com</p>
+      <div className="flex gap-1 max-sm:flex-col sm:gap-6">
+        <p>Contact Us</p>
+        <p>Privacy</p>
+        <p>Terms</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
