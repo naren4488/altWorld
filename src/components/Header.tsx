e352c2e6e1b38ef1a34e3bce8b3dddaeaaf9fb17
naren4488 <narendrakajla77@gmail.com>
2024-05-04 23:40:41 +0530

    header implemented

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,12 @@
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="mb-3">
+      <span className=" text-sm text-gray-500">
+        Pages / <span className="text-black">Assignment</span>
+      </span>
+      <h4 className=" font-semibold">Sales BDE</h4>
+    </div>
+  );
 };
 
 export default Header;
