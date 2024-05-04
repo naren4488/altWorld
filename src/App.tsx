5e737c5ee335d4415c62af7de49ae11ebabfb4f9
naren4488 <narendrakajla77@gmail.com>
2024-05-04 23:32:32 +0530

    project setup with vite-react & shadcn UI

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import AssignmentPage from "./pages/AssignmentPage";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-[#F8F9FA]">
+      <AssignmentPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/AssignmentPage.tsx
@@ -0,0 +1,14 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+const AssignmentPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col p-4">
+      <Header />
+      <div className="flex-1">Main content</div>
+      <Footer />
+    </div>
+  );
+};
+
+export default AssignmentPage;
